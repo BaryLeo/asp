3e374cc3e0b1ff15f27bdc8687e6f12c823237a7
Leo <752992071@qq.com>
2019-12-24 22:00:35 +0800

    fix

---
+++ WebSite1/App_Code/Dao/UserDao.cs
@@ -0,0 +1,59 @@
+﻿using Dao;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// UserDao 的摘要说明
+/// </summary>
+public class UserDao
+{
+    DataClassesDataContext data = new DataClassesDataContext();
+    
+    public UserDao()
+    {
+        
+    }
+    //
+    // 返回1为存储成功，返回0为失败
+    //
+    public int saveUser(User u)
+    {
+
+        try
+        {
+            data.User.InsertOnSubmit(u);
+            data.SubmitChanges();
+            return 1;
+        }
+        catch
+        {
+
+        }
+        return 0;
+    }
+
+    //通过ID获取User信息
+    public User getUser(string id)
+    {
+        var dbUser = from tbUser in data.User
+                     where tbUser.id.Equals(id)
+                     select new
+                     {
+                         tbUser.id,
+                         tbUser.password,
+                         tbUser.isComstomer
+                     };
+
+        User u = new User();
+        foreach (var i in dbUser)
+        {
+            u.id = i.id;
+            u.password = i.password;
+            u.isComstomer = i.isComstomer;
+        }
+            return u;
+    }
+
+}
---
+++ WebSite1/testSQL.aspx
@@ -0,0 +1,17 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="testSQL.aspx.cs" Inherits="testSQL" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Button ID="Button1" runat="server" Text="登录" class="login-button" OnClick="Button1_Click" /><br/>
+        </div>
+    </form>
+</body>
+</html>
