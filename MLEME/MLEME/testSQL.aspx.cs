f82debc6277bea0ffe5057117251502db6672007
Leo <752992071@qq.com>
2019-12-25 20:30:25 +0800

    fix

---
+++ MLEME/MLEME/testSQL.aspx.cs
@@ -0,0 +1,22 @@
+﻿using Dao;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class testSQL : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        UserDao userDao = new UserDao();
+        User user = userDao.getUser("123456");
+        Response.Write("ok+" + user.password);
+    }
+}
